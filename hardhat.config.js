c957ae40e23c1a753963a12580ce6e48b381cf80
Nimisha Agarwal <agarwalnimi626@gmail.com>
2024-05-04 15:32:52 -0500

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/DasVYgjHNhxpdYJO7D7hX4coRyVMg6nY",
+      accounts: [ "695074dee7b545a2192a22262f5bc5bed1bb77976611607d253666527313d091" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export const GetIpfsUrlFromPinata = (pinataUrl) => {
+    console.log("pinataUrl", pinataUrl);
+
+    // Check if pinataUrl is a valid string
+    if (typeof pinataUrl !== 'string' || !pinataUrl.trim()) {
+        // Handle invalid input (e.g., return null or throw an error)
+        console.error('Invalid pinataUrl:', pinataUrl);
+        return null;
+    }
+
+    // Split the pinataUrl by "/"
+    const IPFSUrlParts = pinataUrl.split("/");
+
+    // Extract the last part (IPFS hash)
+    const IPFSHash = IPFSUrlParts[IPFSUrlParts.length - 1];
+
+    // Construct the IPFS URL using cf-ipfs.com gateway
+    const IPFSUrl = `https://cf-ipfs.com/ipfs/${IPFSHash}`;
+
+    return IPFSUrl;
+};
