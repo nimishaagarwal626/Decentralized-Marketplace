85698f3c901eaabc741e56dd8401a6436adce1ab
Nimisha Agarwal <agarwalnimi626@gmail.com>
2024-05-05 18:29:11 -0500

    Added Readme file

--- README.md
+++ README.md
@@ -1,11 +1,76 @@
-# Basic NFT Marketplace end to end
+# DECENTRALIZED-MARKETPLACE
+Welcome to the decentralized NFT marketplace built on the Ethereum blockchain using Solidity. This marketplace allows users to list and purchase non-fungible tokens (NFTs) securely and transparently. The smart contract powering this marketplace is deployed on the Sepolia testnet network, and transactions are executed using the MetaMask wallet. On executio of buy and sell transaction, transaction id can be obtained and verified in sepolia.etherscan.io
 
-This code is for the Tutorial [Build your own NFT Marketplace from Scratch](https://docs.alchemy.com/alchemy/) built by [alchemy.com](https://alchemy.com)
+# Website Link: https://decmarketplace.netlify.app/
 
-To set up the repository and run the marketplace locally, run the below
-```bash
-git clone https://github.com/OMGWINNING/NFT-Marketplace-Tutorial
-cd NFT-Marketplace-Tutorial
-npm install
-npm start
-```# Decentralized-Marketplace
+## Techstack used:
+- Frontend: React JS, Tailwind CSS
+- Contract: Solidity
+- Wallet: Metamask
+- Ethers used: Sepolia Testnet
+- Solidity Deployment: Hardhat
+- Application Deployment: Netlify
+
+# Setup/Commands Used:
+- npm install
+- Account created in Alchemy to obtain sepoia https url
+- Account created in Pinata for file upload and used ipfs for storage.
+- npx hardhat run --networks sepolia scripts/deploy.js
+- npm start
+
+## Solidity Development:
+This Solidity smart contract is present in contracts/NFTMarketplace.sol that allows users to:
+- Create and list NFTs for sale.
+- Browse and purchase listed NFTs.
+- Manage ownership and pricing of NFTs in the marketplace.
+- Functionalities:
+    -> createToken(string tokenURI, uint256 price): Creates a new NFT and lists it for sale.
+    -> getAllNFTs(): Retrieves all NFTs currently listed on the marketplace.
+    -> getMyNFTs(): Retrieves NFTs owned or listed by the caller.
+    -> executeSale(uint256 tokenId): Executes a purchase of a listed NFT.
+- Requirements: 
+    -> Solidity Compiler: Use Solidity version ^0.8.0.
+    -> Hardhat: For development and testing.
+    -> OpenZeppelin Contracts: Utilized for ERC721 token and extensions.
+
+To list the item in the marketplace, solidity sets up the initial listing price to be 0.001 ethers.
+To deploy the contract, deployment script is located in scripts/deploy.js
+- Deployment: npx hardhat run --networks sepolia scripts/deploy.js
+
+## Frontend Development:
+### Connect Wallet
+The Navbar component manages user wallet connection status and Ethereum address using React state variables. It enables wallet connection through MetaMask, leveraging React Router for navigation.
+
+### List NFT
+The ListNFT section allows users to list new NFTs within the marketplace. It integrates with Pinata for IPFS integration, uploading NFT metadata and images securely to IPFS. Key features include:
+- File upload using Pinata for decentralized storage.
+- Interaction with Ethereum contracts using ethers.js for NFT listing.
+
+### Buying
+The NFTPage component facilitates NFT purchases within the marketplace. It interacts with the blockchain to execute transactions for purchasing listed NFTs. Features include:
+- Formatting sale price in Wei using ethers.js.
+- Real-time feedback and transaction ID display upon successful purchase.
+
+### Marketplace
+The Marketplace section displays all listed NFTs within the marketplace application. Features include:
+- Integration with Ethereum for fetching NFT data from the smart contract.
+- IPFS integration via Pinata for fetching metadata and images.
+
+### Profile
+The Profile section displays user-specific NFT data within the marketplace. It integrates with Ethereum to fetch user-owned NFTs and associated metadata. Features include:
+- Display of user wallet address, total NFT value, and owned NFTs.
+- Error handling for transparent user feedback.
+
+
+## Code Deployment
+The code is deployed into production using Netlify. The Github account is connected to Netlify and on pushing any change to the github triggers automatic deployment.
+
+## Challenges Faced And Resolution:
+- Search for IPFS integration: I was trying to use infura ipfs but did not become successful. To figure out a different IPFS was a big task but then understanding pinata and its usage resolved the issue.
+
+- After an NFT was sold, It was again getting listed for buying - After going through the sol file again and on execution of Sale, setting currentlyListed boolean to false and then accessing it on frontend resolved this issue.
+
+# Notes:
+- After selecting the file in list an NFT page, you must wait for a few seconds until it gets uploaded to Pinata. On Clicking list NFT immediately may cause upload error. If so click list NFT again.
+
+- Public IPFS gateway may become unavailable sometimes which may cause application to not work. But we can always update the gateway url in utils.js file and the website would start working.
--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -39,44 +39,63 @@ contract NFTMarketplace is ERC721URIStorage {
         owner = payable(msg.sender);
     }
 
-    function updateListPrice(uint256 _listPrice) public payable {
-        require(owner == msg.sender, "Only owner can update listing price");
-        listPrice = _listPrice;
-    }
-
+    // Function to retrieve the current listing price
     function getListPrice() public view returns (uint256) {
         return listPrice;
     }
 
+    // Function to get details of the latest listed token
     function getLatestIdToListedToken() public view returns (ListedToken memory) {
         uint256 currentTokenId = _tokenIds.current();
         return idToListedToken[currentTokenId];
     }
 
+    // Function to get details of a specific listed token by tokenId
     function getListedTokenForId(uint256 tokenId) public view returns (ListedToken memory) {
         return idToListedToken[tokenId];
     }
 
+     // Function to get the current tokenId being created
     function getCurrentToken() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-    //The first time a token is created, it is listed here
+      /**
+     * @notice Creates a new NFT and lists it for sale with the specified tokenURI and price.
+     * @dev Mints a new NFT, assigns the tokenURI metadata, and lists it for sale in the marketplace.
+     *      Transfers the ownership of the newly created NFT to the marketplace contract.
+     * @param tokenURI The URI for the metadata of the new NFT.
+     * @param price The listing price for the new NFT.
+     * @return The ID of the newly created NFT.
+     */
     function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
 
+        // Mint a new NFT with the newly generated token ID and assign the tokenURI metadata
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
+
+        // List the newly created NFT for sale in the marketplace with the specified price
         createListedToken(newTokenId, price);
 
         return newTokenId;
     }
 
+    /**
+     * @notice Creates a new listing for an NFT with the specified tokenId and price.
+     * @dev Marks the specified NFT as listed for sale with the given price.
+     *      Transfers the ownership of the NFT to the marketplace contract.
+     * @param tokenId The ID of the NFT to be listed for sale.
+     * @param price The listing price for the NFT.
+     */
     function createListedToken(uint256 tokenId, uint256 price) private {
+        // Validate that the sent value matches the expected listing price
         require(msg.value == listPrice, "Hopefully sending the correct price");
+        // Validate that the listing price is greater than zero
         require(price > 0, "Make sure the price isn't negative");
 
+        // Create a new ListedToken struct for the specified tokenId and price
         idToListedToken[tokenId] = ListedToken(
             tokenId,
             payable(address(this)),
@@ -85,6 +104,7 @@ contract NFTMarketplace is ERC721URIStorage {
             true
         );
 
+        // Transfer ownership of the NFT to the marketplace contract
         _transfer(msg.sender, address(this), tokenId);
         emit TokenListedSuccess(
             tokenId,
@@ -95,46 +115,71 @@ contract NFTMarketplace is ERC721URIStorage {
         );
     }
     
-    //This will return all the NFTs currently listed to be sold on the marketplace
+        /**
+     * @notice Retrieves all NFTs currently listed for sale in the marketplace.
+     * @dev Iterates through all listed tokens to collect their details into an array.
+     * @return An array of ListedToken structs representing all currently listed NFTs.
+     */
     function getAllNFTs() public view returns (ListedToken[] memory) {
+        // Get the total number of tokens currently in existence
         uint nftCount = _tokenIds.current();
+         // Initialize an array to store all listed tokens
         ListedToken[] memory tokens = new ListedToken[](nftCount);
+
         uint currentIndex = 0;
         uint currentId;
+
+         // Iterate through all tokens to collect their details
         for(uint i=0;i<nftCount;i++)
         {
             currentId = i + 1;
-            ListedToken storage currentItem = idToListedToken[currentId];
-            tokens[currentIndex] = currentItem;
+            ListedToken storage currentItem = idToListedToken[currentId]; // Retrieve the details of the current token from the idToListedToken mapping
+            tokens[currentIndex] = currentItem; // Store the current token details in the tokens array at the currentIndex
             currentIndex += 1;
         }
         return tokens;
     }
     
-    //Returns all the NFTs that the current user is owner or seller in
+        /**
+     * @notice Retrieves all NFTs owned or listed by the caller.
+     * @dev Iterates through all listed tokens to find those owned or listed by the caller.
+     * @return An array of ListedToken structs representing the caller's owned or listed NFTs.
+     */
     function getMyNFTs() public view returns (ListedToken[] memory) {
+        // Get the total number of tokens currently in existence
         uint totalItemCount = _tokenIds.current();
+        // Initialize counters and variables for processing
         uint itemCount = 0;
         uint currentIndex = 0;
         uint currentId;
+
+        // Loop through all tokens to count those owned or listed by the caller
         for(uint i=0; i < totalItemCount; i++)
         {
             if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender){
                 itemCount += 1;
             }
         }
+        // Initialize an array to store the caller's owned or listed tokens
         ListedToken[] memory items = new ListedToken[](itemCount);
+
+        // Loop through all tokens again to populate the items array with caller's tokens
         for(uint i=0; i < totalItemCount; i++) {
             if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender) {
                 currentId = i+1;
-                ListedToken storage currentItem = idToListedToken[currentId];
-                items[currentIndex] = currentItem;
+                ListedToken storage currentItem = idToListedToken[currentId]; // Get the token details
+                items[currentIndex] = currentItem; // Store the token in the items array
                 currentIndex += 1;
             }
         }
         return items;
     }
 
+    /**
+     * @notice Executes the purchase of a listed NFT.
+     * @dev Transfers the ownership of the specified NFT to the buyer and handles the fund transfer.
+     * @param tokenId The ID of the NFT to be purchased.
+     */
     function executeSale(uint256 tokenId) public payable {
         uint price = idToListedToken[tokenId].price;
         address seller = idToListedToken[tokenId].seller;
@@ -149,6 +194,7 @@ contract NFTMarketplace is ERC721URIStorage {
         _transfer(address(this), msg.sender, tokenId);
         approve(address(this), tokenId);
 
+        // Transfer funds to seller and contract owner
         payable(owner).transfer(listPrice);
         payable(seller).transfer(msg.value);
     }
